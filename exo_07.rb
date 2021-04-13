b7db290e504896534ab75e4126d6ccea40d9c14c
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-04-13 10:53:00 +0200

    fromage

---
+++ exo_07.rb
@@ -0,0 +1,11 @@
+# Écris un programme exo_07.rb qui demande un nombre à l'utilisateur, puis qui compte jusqu'à ce nombre.
+
+
+puts " Quel nombre saisissez vous ?"
+number = gets.to_i
+
+i = 0
+while number > i
+   puts i = i + 1
+    
+end
