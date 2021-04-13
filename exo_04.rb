cf8d8f3602cc64d067338e1565dc35875fc74c78
tony31490 <tony31490@gmail.com>
2021-04-13 10:20:38 +0200

    exo 4 fini

---
+++ exo_04.rb
@@ -0,0 +1,10 @@
+#Écris un programme exo_04.rb qui demande son année de naissance à l'utilisateur, puis qui ressort l'année où l'utilisateur aura 100 ans.
+
+puts "en quel année est tu née?"
+puts "<"
+
+baguette = gets.chomp.to_i
+
+fromage = 100
+
+puts "#{baguette + fromage} est l'année ou il aura 100an"
