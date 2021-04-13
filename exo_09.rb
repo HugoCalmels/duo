a8673b690c430cc86117e6330c41a0236255567f
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-04-13 11:33:28 +0200

    je suis con

---
+++ exo_09.rb
@@ -0,0 +1,12 @@
+# Écris un programme exo_09.rb qui demande son année de naissance à l'utilisateur, puis qui va ressortir chaque année depuis son année de naissance jusqu'aujourd'hui.
+
+
+puts " Saisir votre année de naissance"
+puts ">"
+
+
+i = gets.chomp.to_i
+
+while i < 2021
+    puts i = i + 1
+end
