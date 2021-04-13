78362cbfe357bd17d775f85f78f2aef4fd639901
tony31490 <tony31490@gmail.com>
2021-04-13 11:44:15 +0200

    exo 10 fini

---
+++ exo_10.rb
@@ -0,0 +1,14 @@
+#Écris un programme exo_10.rb qui demande son année de naissance à l'utilisateur et qui va afficher chaque année depuis son année de naissance jusqu'aujourd'hui. 
+#Pour chaque année affichée, le programme devra annoncer l'âge que l'utilisateur avait cette année-là.
+
+
+puts " Saisir votre année de naissance"
+puts ">"
+
+i = gets.chomp.to_i
+d = 0
+
+while i < 2021
+    puts"en #{i = i + 1}"" j ai #{d = d+1} an"
+    
+end
